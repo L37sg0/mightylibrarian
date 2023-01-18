9923b55e74e0d70d35fe568e948835f391754c3e
l37sg0 <petar_1v@abv.bg>
2023-01-18 06:40:21 +0200

    [#4]  - [Author]  - separated views for index and new-or-edit - 50 lines less code.

--- mightylibrarian/resources/views/admin/author/index.blade.php
+++ mightylibrarian/resources/views/admin/author/index.blade.php
@@ -1,5 +1,7 @@
 @php
 use App\Models\Author\Author as Model;
+
+$author = null;
 @endphp
 @extends('admin.layouts.app')
 
@@ -12,45 +14,7 @@ use App\Models\Author\Author as Model;
                     <h2>{{__('All Authors')}}</h2>
                 </div>
                 <div class="offset-md-7 col-md-2">
-                    <button type="button" class="btn btn-link" data-bs-toggle="modal"
-                            data-bs-target="#authorCreateViewEdit">{{__('Add New')}}
-                    </button>
-                    <div class="modal fade"
-                         id="authorCreateViewEdit"
-                         tabindex="-1"
-                         aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content rounded-4 shadow">
-                                <div class="modal-header p-5 pb-4 border-bottom-0">
-                                    <h1 class="fw-bold mb-0 fs-2">{{__('New Author')}}</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                </div>
-                                <form role="form"
-                                      class="form-hidden"
-                                      action="{{route('dashboard.authors.create')}}"
-                                      method="post">
-                                    @csrf
-                                    <div class="modal-body p-5 pt-0">
-                                        <div class="form-floating mb-3">
-                                            <input type="text"
-                                                   class="form-control rounded-3"
-                                                   id="name"
-                                                   name="name"
-                                                   placeholder="{{__('Author Name')}}">
-                                            <label for="name">{{__('Author Name')}}</label>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">{{__('Close')}}
-                                        </button>
-                                        <button type="submit" class="btn btn-primary">{{__('Save')}}</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                    @include('admin.author.new-or-edit')
                 </div>
             </div>
             <div class="table-responsive">
@@ -68,10 +32,7 @@ use App\Models\Author\Author as Model;
                         <td>{{$author->getAttribute(Model::FIELD_ID)}}</td>
                         <td>{{$author->getAttribute(Model::FIELD_NAME)}}</td>
                         <td>
-
-                            <button type="button" class="btn btn-link" data-bs-toggle="modal"
-                                    data-bs-target="#authorCreateViewEdit{{$author->getAttribute(Model::FIELD_ID)}}">Edit
-                            </button>
+                            @include('admin.author.new-or-edit')
                             <form action="{{route('dashboard.authors.delete',$author)}}" method="post"
                                   class="form-hidden">
                                 @csrf
@@ -79,38 +40,6 @@ use App\Models\Author\Author as Model;
                             </form>
                         </td>
                     </tr>
-                    <div class="modal fade" id="authorCreateViewEdit{{$author->getAttribute(Model::FIELD_ID)}}" tabindex="-1" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content rounded-4 shadow">
-                                <div class="modal-header p-5 pb-4 border-bottom-0">
-                                    <h1 class="fw-bold mb-0 fs-2">{{__('Edit Author')}}</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                </div>
-                                <form role="form"
-                                      class="form-hidden"
-                                      action="{{route('dashboard.authors.update', $author)}}"
-                                      method="post">
-                                    @csrf
-                                    <div class="modal-body p-5 pt-0">
-                                        <div class="form-floating mb-3">
-                                            <input type="text"
-                                                   class="form-control rounded-3"
-                                                   id="name"
-                                                   name="name"
-                                                   value="{{$author->getAttribute(Model::FIELD_NAME)}}">
-                                            <label for="name">{{__('Author Name')}}</label>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}
-                                        </button>
-                                        <button type="submit" class="btn btn-primary">{{__('Save')}}</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
                     @empty
                         <tr>
                             <td colspan="3" class="text-center">{{__('No Authors Found')}}</td>
---
+++ mightylibrarian/resources/views/admin/author/new-or-edit.blade.php
@@ -0,0 +1,45 @@
+@php
+use App\Models\Author\Author;
+@endphp
+
+<button type="button" class="btn btn-link" data-bs-toggle="modal"
+        data-bs-target="#authorCreateViewEdit{{($author) ? $author->getAttribute(Author::FIELD_ID) : ''}}">
+    {{($author) ? __('Edit') : __('Add New')}}
+</button>
+<div class="modal fade"
+     id="authorCreateViewEdit{{($author) ? $author->getAttribute(Author::FIELD_ID) : ''}}"
+     tabindex="-1"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content rounded-4 shadow">
+            <div class="modal-header p-5 pb-4 border-bottom-0">
+                <h1 class="fw-bold mb-0 fs-2">{{($author) ? __('Edit Author') : __('New Author')}}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+            </div>
+            <form role="form"
+                  class="form-hidden"
+                  action="{{($author) ? route('dashboard.authors.update', $author) : route('dashboard.authors.create')}}"
+                  method="post">
+                @csrf
+                <div class="modal-body p-5 pt-0">
+                    <div class="form-floating mb-3">
+                        <input type="text"
+                               class="form-control rounded-3"
+                               id="name"
+                               name="name"
+                               placeholder="{{__('Author Name')}}"
+                                value="{{($author) ? $author->getAttribute(Author::FIELD_NAME) : ''}}">
+                        <label for="name">{{__('Author Name')}}</label>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary"
+                            data-bs-dismiss="modal">{{__('Close')}}
+                    </button>
+                    <button type="submit" class="btn btn-primary">{{__('Save')}}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
